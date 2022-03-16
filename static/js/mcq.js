5efa5a1280bd3c25c9deafcc0a266b271dc0d5ec
Sankha Subhra Mondal <sankhasubhramondal18@gmail.com>
2022-03-17 00:41:07 +0530

    Add files via upload

---
+++ static/js/mcq.js
@@ -0,0 +1,117 @@
+
+
+function containerData(){
+    $('.sada-container').html(`
+    <div class="q-set">
+
+    <div class="question-format">
+    
+      <span class="q-sl">1.</span>
+      <span class="q">Why was plastic invented?</span>
+      
+    
+    </div>
+    <div class="answer-format">
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel1" value="" aria-label="" />
+        <label class="form-check-label" for="inlineRadio1">To find an alternative material to paper and fabric </label>
+      </div>
+      
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel2" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio2">To find an affordable material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel3" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio3">To find an unreactive material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel4" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio4">To make stronger material </label>
+      </div>
+    </div>
+
+    
+
+  </div>
+  
+
+  <div class="q-set">
+
+    <div class="question-format">
+    
+        <span class="q-sl">2.</span>
+        <span class="q">Why was plastic invented?</span>
+        
+      
+    </div>
+    <div class="answer-format">
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel5" value="" aria-label="" />
+        <label class="form-check-label" for="inlineRadio5">To find an alternative material to paper and fabric </label>
+      </div>
+      
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel6" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio6">To find an affordable material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel7" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio7">To find an unreactive material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel8" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio8">To make stronger material </label>
+      </div>
+    </div>
+
+    
+
+  </div>
+
+  <div class="q-set">
+
+    <div class="question-format">
+    
+        <span class="q-sl">3.</span>
+        <span class="q">Why was plastic invented?</span>
+        
+      
+    </div>
+    <div class="answer-format">
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel5" value="" aria-label="" />
+        <label class="form-check-label" for="inlineRadio5">To find an alternative material to paper and fabric </label>
+      </div>
+      
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel6" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio6">To find an affordable material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel7" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio7">To find an unreactive material</label>
+      </div>
+      <div class="options">
+        <input class="form-check-input" type="radio" name="radioNoLabel" id="radioNoLabel8" value="" aria-label="..." />
+        <label class="form-check-label" for="inlineRadio8">To make stronger material </label>
+      </div>
+    </div>
+
+    
+
+  </div>
+
+
+  <div class="controller" style="padding:25px;">
+    <button class="btn btn-block btn-info" style="font-weight: bold;">Back</button>
+    <button class="btn btn-block btn-warning" style="margin:0 auto;float:right;font-weight: bold;">Submit</button>
+  </div>`);
+
+
+
+}
+
+
+
+setInterval(containerData,4000)
